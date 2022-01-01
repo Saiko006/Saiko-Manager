9ec06b024b84b3a63d5a348ff58cc52189517295
Zal-EX <94663108+izalmusic@users.noreply.github.com>
2022-01-01 17:17:20 +0700

    Update misc.py

--- Yuriko/modules/helper_funcs/misc.py
+++ Yuriko/modules/helper_funcs/misc.py
@@ -74,9 +74,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     else:
         pairs += [
                   [
-                    EqInlineKeyboardButton("╰✰ Cʜᴀɴɴᴇʟ", url="t.me/Deecodebots"), 
+                    EqInlineKeyboardButton("╰✰ Cʜᴀɴɴᴇʟ", url="t.me/musickekiniaan"), 
                     EqInlineKeyboardButton("✗ Bᴀᴄᴋ ✗", callback_data="yurikorobot_back"), 
-                    EqInlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ ✰╮", url="t.me/Decodesupport")
+                    EqInlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ ✰╮", url="t.me/Kekiniangroup")
                   ]
                  ]
 
