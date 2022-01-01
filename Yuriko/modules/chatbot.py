66864fa53035d98df69049ba9d73172891b033bf
Zal-EX <94663108+izalmusic@users.noreply.github.com>
2022-01-01 11:14:05 +0700

    Update chatbot.py

--- Yuriko/modules/chatbot.py
+++ Yuriko/modules/chatbot.py
@@ -138,13 +138,13 @@ def list_all_chats(update: Update, context: CallbackContext):
     update.effective_message.reply_text(text, parse_mode="HTML")
 
 __help__ = """
-✗ `Chatbot utilizes the` *YURIKO* `api which allows Yuriko to talk and provide a more interactive group chat experience.`
+✗ `Chatbot utilizes the` *KEIKINIAN* `api which allows Yuriko to talk and provide a more interactive group chat experience.`
 
 *Admins only Commands*:
  
 ✗ /Chatbot - `Shows chatbot control panel`
   
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ!*
 """
 
 __mod_name__ = "CʜᴀᴛBᴏᴛ"
