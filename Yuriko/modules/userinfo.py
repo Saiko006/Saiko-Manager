0345b53d1fbe369d4ab72630af08e4056a471e16
Zal-EX <94663108+izalmusic@users.noreply.github.com>
2022-01-01 19:55:26 +0700

    Update userinfo.py

--- Yuriko/modules/userinfo.py
+++ Yuriko/modules/userinfo.py
@@ -539,47 +539,47 @@ def __user_info__(user_id):
 
 __help__ = """
 *ID:*
-✗ /id - `get the current group id. If used by replying to a message, gets that user's id.`
+✪ /id - `get the current group id. If used by replying to a message, gets that user's id.`
 
-✗ /gifid - `reply to a gif to me to tell you its file ID.`
+✪ /gifid - `reply to a gif to me to tell you its file ID.`
  
 *Self addded information:* 
 
-✗ /setme - `<text> will set your info.`
+✪ /setme - `<text> will set your info.`
 
-✗ /me - `will get your or another user's info.`
+✪ /me - `will get your or another user's info.`
 
 Examples:
 
-✗ /setme - `I am a` *DᴇCᴏᴅᴇ* `Member.`
+✪ /setme - `I am a` *KEKINIAN* `Member.`
 
-✗ /me - `@username(defaults to yours if no user specified)`
+✪ /me - `@username(defaults to yours if no user specified)`
  
 *Information others add on you:* 
 
-✗ /bio - `will get your or another user's bio. This cannot be set by yourself.`
+✪ /bio - `will get your or another user's bio. This cannot be set by yourself.`
 
-✗ /setbio - `<text> while replying, will save another user's bio`
+✪ /setbio - `<text> while replying, will save another user's bio`
 
 *Examples:*
 
-✗ /bio - `@username(defaults to yours if not specified).`
+✪ /bio - `@username(defaults to yours if not specified).`
 
-✗ /setbio - `This user is a` *DᴇCᴏᴅᴇ* `Member (reply to the user)`
+✪ /setbio - `This user is a` *KEKINIAN* `Member (reply to the user)`
  
 *Overall Information about you:*
 
-✗ /info - `get information about a user.`
+✪ /info - `get information about a user.`
  
 *json Detailed info:*
 
-✗ /json - `Get Detailed info about any message.`
+✪ /json - `Get Detailed info about any message.`
  
 *AFk:*
 
 `When marked as AFK, any mentions will be replied to with a message stating that you're not available!`
 
-✗ /afk - `<reason> Mark yourself as AFK.`
+✪ /afk - `<reason> Mark yourself as AFK.`
 
   brb - `<reason> Same as the afk command, but not a command.` 
 
@@ -587,7 +587,7 @@ Examples:
 
  `Come and see` [HP System explained](https://t.me/YurikoLogger/5)
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✪ Pᴏᴡᴇʀᴇᴅ 💎 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ!*
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio, run_async=True)
