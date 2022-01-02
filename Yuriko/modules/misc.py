0ee6f1bded9ff424eec7a1bb51a453496de8194b
Zal-EX <94663108+izalmusic@users.noreply.github.com>
2022-01-02 11:29:37 +0700

    Update misc.py

--- Yuriko/modules/misc.py
+++ Yuriko/modules/misc.py
@@ -35,17 +35,17 @@ MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
 
-✗ <code>_italic_</code> - `wrapping text with '_' will produce italic text`
+✪ <code>_italic_</code> - `wrapping text with '_' will produce italic text`
 
-✗ <code>*bold*</code> - `wrapping text with '*' will produce bold text`
+✪ <code>*bold*</code> - `wrapping text with '*' will produce bold text`
 
-✗ <code>`code`</code> - `wrapping text with '`' will produce monospaced text, also known as code`
+✪ <code>`code`</code> - `wrapping text with '`' will produce monospaced text, also known as code`
 
-✗ <code>[sometext](someURL)</code> - `this will create a link - the message will just show <code>sometext</code>, \
+✪ <code>[sometext](someURL)</code> - `this will create a link - the message will just show <code>sometext</code>, \
 and tapping on it will open the page at <code>someURL</code>.
 <b>Example:</b><code>[test](example.com)</code>`
 
-✗ <code>[buttontext](buttonurl - `someURL)</code>: this is a special enhancement to allow users to have telegram \
+✪ <code>[buttontext](buttonurl - `someURL)</code>: this is a special enhancement to allow users to have telegram \
 buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
 will be the url which is opened.`
 <b>Example:</b> <code>[This is a button](buttonurl - `example.com)</code>`
@@ -219,14 +219,17 @@ __help__ = """
 
 
 *Music Modules:*
+✪ /play (query) - `play song from youtube`
 
-✗ /video or /vsong (query) - `download video from youtube`
+✪ /settings - `settings volume dan authorized`
 
-✗ /music or /song (query)- `download song from yt servers. (API BASED)`
+✪ /video or /vsong (query) - `download video from youtube`
 
-✗ /lyrics (song name) - `This plugin searches for song lyrics with song name.`
+✪ /music or /song (query)- `download song from yt servers. (API BASED)`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ 🤖!*
+✪ /lyrics (song name) - `This plugin searches for song lyrics with song name.`
+
+*✪ Pᴏᴡᴇʀᴇᴅ 💎 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ!*
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler(
