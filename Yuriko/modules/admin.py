d05c624234e044bb9c8c0c06e5c00d6f4d14a416
Zal-EX <94663108+izalmusic@users.noreply.github.com>
2022-01-23 17:07:51 +0700

    Update admin.py

--- Yuriko/modules/admin.py
+++ Yuriko/modules/admin.py
@@ -1001,7 +1001,7 @@ SETCHATPIC_HANDLER = CommandHandler("setgpic", setchatpic, filters=Filters.chat_
 RMCHATPIC_HANDLER = CommandHandler("delgpic", rmchatpic, filters=Filters.chat_type.groups, run_async=True)
 SETCHAT_TITLE_HANDLER = CommandHandler("setgtitle", setchat_title, filters=Filters.chat_type.groups, run_async=True)
 
-ADMINLIST_HANDLER = DisableAbleCommandHandle(["admins", "staff"]), adminlist, run_async=True
+ADMINLIST_HANDLER = DisableAbleCommandHandle(["admins", "staff"]), adminlist, run_async=True)
 
 PIN_HANDLER = CommandHandler("pin", pin, filters=Filters.chat_type.groups, run_async=True)
 UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.chat_type.groups, run_async=True)
