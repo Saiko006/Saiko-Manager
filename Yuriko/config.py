8dd538adedba38789183e5751d78611bdb8a13cd
izal <94070031+Izalmirage@users.noreply.github.com>
2022-02-02 18:29:36 +0700

    Update config.py

--- Yuriko/config.py
+++ Yuriko/config.py
@@ -17,7 +17,6 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     ARQ_API_URL = "https://thearq.tech"
-    ARQ_API = WOBAJD-PIWJYU-SSHBQS-GJWIAN-ARQ #This var used API_ARQ
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2075788563  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "rumahakhirat"
