a59f749f7782d4bd885f0236cea8dec44deca512
Zal <94663108+izalmusic@users.noreply.github.com>
2022-02-05 22:52:37 +0700

    Update phub.py

--- Yuriko/modules/phub.py
+++ Yuriko/modules/phub.py
@@ -26,7 +26,7 @@ def get_text(message) -> [None, str]:
         return None
 
 # Let's Go----------------------------------------------------------------------
-@bot1.on_message(filters.command(["phub"]) & ~filters.edited & filters.private)
+@bot1.on_message(filters.command(["phub"]) & ~filters.edited & ~filters.private & ~filters.bot)
 async def sarch(_, message):
     search = get_text(message)
     if message.chat.username:
