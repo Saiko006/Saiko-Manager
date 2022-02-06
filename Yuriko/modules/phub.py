db2c4af871777908b4fe38ceff0f065c3e40c8b4
Zal <94663108+izalmusic@users.noreply.github.com>
2022-02-06 20:14:34 +0700

    Update phub.py

--- Yuriko/modules/phub.py
+++ Yuriko/modules/phub.py
@@ -50,6 +50,7 @@ async def sarch(_, message):
             ),
         )
         
+    m = message.reply_to_message 
     search = message.text   
     try:
         resp = await pornhub(search, thumbsize="large_hd")
