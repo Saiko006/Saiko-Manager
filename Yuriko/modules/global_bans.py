4e301cad682b40e4e6b785279b78c2c2712fad60
Zal <94663108+izalmusic@users.noreply.github.com>
2022-02-10 13:54:57 +0700

    Update global_bans.py

--- Yuriko/modules/global_bans.py
+++ Yuriko/modules/global_bans.py
@@ -542,24 +542,8 @@ def __migrate__(old_chat_id, new_chat_id):
 def __chat_settings__(chat_id, user_id):
     return f"This chat is enforcing *gbans*: `{sql.does_chat_gban(chat_id)}`."
 
-
-__help__ = f"""
-*Admins only:*
-
-✪ /antispam - `<on/off/yes/no> Will toggle our antispam tech or return your current settings.`
-
-`Anti-Spam used by bot devs to ban spammers across all groups. This helps protect`
-`you and your groups by removing spam flooders as quickly as possible.`
-*Note:* `Users can appeal gbans or report spammers at` @{SUPPORT_CHAT}
-
-`This also integrates` @Spamwatch `API to remove Spammers as much as possible from your chatroom!`
-*What is SpamWatch?*
-`SpamWatch maintains a large constantly updated ban-list of spambots trolls bitcoin spammers and unsavoury characters`
-`Constantly help banning spammers off from your group automatically So you wont have to worry about spammers storming your group.`
-*Note:* `Users can appeal spamwatch bans at` @Spamwatch
-
-*✪ Pᴏᴡᴇʀᴇᴅ 💎 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ!*
-"""
+def helps(chat):
+    return gs(chat, "antispam_help")
 
 GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
 UNGBAN_HANDLER = CommandHandler("ungban", ungban, run_async=True)
