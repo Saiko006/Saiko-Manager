e933fa6d58a0ea5a815fa937a916d031585da387
Zal <94663108+izalmusic@users.noreply.github.com>
2022-02-10 15:32:07 +0700

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -105,7 +105,7 @@ for module_name in ALL_MODULES:
     else:
         raise Exception("Can't have two modules with the same name! Please change one")
 
-    if hasattr(imported_module, "__help__") and imported_module.__help__:
+    if hasattr(imported_module, "helps") and imported_module.helps:
         HELPABLE[imported_module.__mod_name__.lower()] = imported_module
 
     # Chats to migrate on chat_migrated events
