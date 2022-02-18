4dac0fd51fcbdc8308251bd33e7f7b44e81b8f5c
izal <94070031+Izalmirage@users.noreply.github.com>
2022-02-18 09:08:36 +0700

    Update approve.py

--- Yuriko/modules/approve.py
+++ Yuriko/modules/approve.py
@@ -213,6 +213,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Aᴘᴘʀᴏᴠᴀʟ"
+__mod_name__ = "Approval"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
