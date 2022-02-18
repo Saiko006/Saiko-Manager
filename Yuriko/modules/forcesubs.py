f95da1d524640f79d6399a266f47637d0533ec02
izal <94070031+Izalmirage@users.noreply.github.com>
2022-02-18 09:31:26 +0700

    Update forcesubs.py

--- Yuriko/modules/forcesubs.py
+++ Yuriko/modules/forcesubs.py
@@ -13,6 +13,7 @@ from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboard
 from Yuriko import DRAGONS as SUDO_USERS
 from Yuriko import pbot
 from Yuriko.modules.sql import forceSubscribe_sql as sql
+from Yuriko.modules.language import gs
 
 logging.basicConfig(level=logging.INFO)
 
@@ -177,37 +178,7 @@ def config(client, message):
         )
 
 
-__help__ = """
-*Force Subscribe:*
+def helps(chat):
+    return gs(chat, "fsub_feds_help")
 
-❂ Kekinian can mute members who are not subscribed your channel until they subscribe
-❂ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-❂*Setup*
-*Only creator*
-❂ Add me in your group as admin
-❂ Add me in your channel as admin 
- 
-*Commmands*
-❂ /fsub {channel username} - To turn on and setup the channel.
-
-  📚Do this first...
-
-❂ /fsub - To get the current settings.
-❂ /fsub disable - To turn of ForceSubscribe..
-
-  📚If you disable fsub, you need to set again for working.. /fsub {channel username} 
-
-❂ /fsub clear - To unmute all members who muted by me.
-
-*Federation*
-Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
-But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
-*No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
-You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
-*Commands:*\n
-Feds are now divided into 3 sections for your ease.
-• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-• `/fedadminhelp`*:* Provides help for fed administration commands
-• `/feduserhelp`*:* Provides help for commands anyone can use
-"""
 __mod_name__ = "F-Sub/Feds"
