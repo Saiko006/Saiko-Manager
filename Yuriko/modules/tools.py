420e6a7e8ddd0eb2e2aaae63115ae0d0816fc316
izal <94070031+Izalmirage@users.noreply.github.com>
2022-02-18 09:26:39 +0700

    Update tools.py

--- Yuriko/modules/tools.py
+++ Yuriko/modules/tools.py
@@ -1,61 +1,14 @@
-__mod_name__ = "Tᴏᴏʟꜱ"
+from Yuriko.modules.language import gs
 
-__help__ = """
-*Kekinian Info bot*
+def helps(chat):
+    return gs(chat, "connection_help")
 
-✪  /sg <reply> - `To check history name`
+__mod_name__ = "Connection"
 
-*Date-time-Weather*
+#tools
+def helps(chat):
+    return gs(chat, "tools_help")
 
-✪  /time - `<country code> Gives information about a timezone.`
+__mod_name__ = "Tools"
 
-✪  /weather - `<city> Get weather info in a particular place.`
 
-✪  /wttr - `<city> Advanced weather module, usage same as /weather`
-
-✪  /wttr - `moon Get the current status of moon`
-
-*Converts*
-
-✪  /encrypt - `Encrypts The Given Text`
-
-✪  /decrypt - `Decrypts Previously Ecrypted Text`
-
-✪  /zip - `reply to a telegram file to compress it in .zip format`
-
-✪  /unzip - `reply to a telegram file to decompress it from the .zip format`
-
-*Random API*
-
-✪  /ptl *or* /asupan - `get random video from Instagram timeline`
-
-✪  /chika - `get random video from chikakiku`
-
-✪  /wibu - `get random short anime video or photos`
-
-✪  /truth - `challenge`
-
-✪  /dare - `challenge`
-
-*Link To File:*
-
-✪  /up - `reply to a direct download link to upload it to telegram as files`
- 
- *File To Link:*
-
-✪  /transfersh - `reply to a telegram file to upload it on transfersh and get direct download link`
-
-✪  /tmpninja - `reply to a telegram file to upload it on tmpninja and get direct download link`
-
- *File Or Text To Telegraph:*
-
-✪  /tgm - `Get Telegraph Link Of Replied Media`
-
-✪  /tgt - `Get Telegraph Link of Replied Text`
-
- *Tagger*
-
-✪  /tagall - `for tag everyone at least 100 member first.`
-
-*✪ Pᴏᴡᴇʀᴇᴅ 💎 Bʏ: ᴋᴇᴋɪɴɪᴀɴ ʀᴏʙᴏᴛ!*
-"""
